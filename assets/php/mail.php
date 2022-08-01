f246ea6357db357c072ac5997d4cbf1e89c9b633
David Gallegos <dgallegosv94@gmail.com>
2022-07-31 23:47:13 -0500

    Update mail.php

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,56 +1,75 @@
 <?php
-
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $subject = trim($_POST["subject"]);
-        $message = trim($_POST["message"]);
-
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Por favor complete el formulario e intente de nuevo.";
-            exit;
-        }
-
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "administracion@lotusadministracion.com";
-
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content = "Subject: $subject\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
-
-        $title    = '! Lotus Website - Informacion de Contacto !';
         
-        // Build the email headers.
-        $email_headers = 'From: LotusWebsite <lotuswebsite.adm@gmail.com>';
-        // $email_headers = "From: $name <$email>";
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
+        
+        require_once "vendor/autoload.php";
+        require_once "constants.php";
+        
+        $mail = new PHPMailer(true);
+    
+        try {
 
-        $enviar = mail($recipient,$title, $email_content, $email_headers);
-        // Send the email.
-        if ($enviar) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Gracias! Tu mensaje ha sido enviado.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            var_dump($enviar);
-            echo "Oops! Algo salió mal y no pudimos enviar tu mensaje.";
+            // Get the form fields and remove whitespace.
+            $name = strip_tags(trim($_POST["name"]));
+            $name = str_replace(array("\r","\n"),array(" "," "),$name);
+            $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+            $subject = trim($_POST["subject"]);
+            $message = trim($_POST["message"]);
+
+            // Check that data was sent to the mailer.
+            if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+                http_response_code(400);
+                echo "Por favor complete el formulario e intente de nuevo.";
+                exit;
+            }
+
+            // Build the email content.
+            $email_content = "Name: $name\n";
+            $email_content = "Subject: $subject\n";
+            $email_content .= "Email: $email\n\n";
+            $email_content .= "Message:\n$message\n";
+
+
+            // Set the recipient email address.
+            // FIXME: Update this to your desired email address.
+            $recipient = "administracion@lotusadministracion.com";
+
+            $mail->isSMTP();
+            $mail->Host = 'smtp.googlemail.com';  //gmail SMTP server
+            $mail->SMTPAuth = true;
+            $mail->Username = "lotuswebsite.adm@gmail.com";   //username
+            $mail->Password = "weayfxtuxswoyvil";   //password
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;                    //smtp port
+        
+            $mail->setFrom('lotuswebsite.adm@gmail.com', 'LotusWebsite');
+            $mail->addAddress($recipient, 'Lotus Administration Website');
+        
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body    = $email_content;
+        
+            $enviar = $mail->send();
+
+            // Send the email.
+            if ($enviar) {
+                // Set a 200 (okay) response code.
+                http_response_code(200);
+                echo "Gracias! Tu mensaje ha sido enviado.";
+            } else {
+                // Set a 500 (internal server error) response code.
+                http_response_code(500);
+                echo "Oops! Algo salió mal y no pudimos enviar tu mensaje.";
+            }
+        
+        } catch (Exception $e) {
+            http_response_code(403);
+            echo 'Message could not be sent. Mailer Error: '. $mail->ErrorInfo;
         }
-
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
     }
-
 ?>
 
