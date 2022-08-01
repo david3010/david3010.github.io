2d09896a351facd03132d90e1b9b30fad62ae033
David Gallegos <dgallegosv94@gmail.com>
2022-07-31 21:14:01 -0500

    Update mail.php

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "example@gmail.com ";
+        $recipient = "administracion@lotusadministracion.com ";
 
         // Build the email content.
         $email_content = "Name: $name\n";
